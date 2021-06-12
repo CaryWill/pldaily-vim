e471ddaa4515896e86c80f62b22170629e7f3df4
cary <CaryWill.K@gmail.com>
2021-06-12 14:40:00 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 # 安装 rg
 brew install ripgrep
 
-# 安装字体
-brew tap homebrew/cask-fonts
-brew cask install font-meslo
-
 # 设置软连接兼容 neovim
 ln -sf ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vimrc ~/.vim/init.vim
